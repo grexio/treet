9ec230e6df8a63b4e0866163f55f2ae31d379bf4
Phaneendra Chiruvella <hi@pcx.io>
2014-05-10 15:49:37 +0530

    adds .dir-locals.el, and sqlite3 database

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+((nil . ((python-django-project-root
+	   . "~/neo/treet-proj/treet/")
+	  (python-django-project-settings
+	   . "web.settings")))
+ (python-mode . ((python-shell-interpreter . "python")
+		 (python-shell-virtualenv-path
+		  . "~/.virtualenvs/treet/'")
+		 (python-shell-interpreter-args
+		  . "~/neo/treet-proj/treet/manage.py shell"))))
